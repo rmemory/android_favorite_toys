2e77e68e86a2762f6235203c25a930d996c66093
Richard Memory <richardfmemory@gmail.com>
2019-04-13 22:17:58 -0600

    Add scroll view

--- app/src/main/java/android/example/com/MainActivity.java
+++ app/src/main/java/android/example/com/MainActivity.java
@@ -2,12 +2,18 @@ package android.example.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        for (String toy: ToyBox.getToyNames()) {
+            mToysListTextView.append(toy + "\n\n\n");
+        }
     }
 }
